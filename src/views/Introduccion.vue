7bb46ecd501c4b4874097c25611939b5dc2c0e59
Andrea Botello <andrea.botellor@misena.edu.co>
2025-08-15 11:41:45 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -37,7 +37,7 @@
             .col-lg-12
               figure
                 .video
-                  iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+                  iframe(width="560" height="315" src="https://www.youtube.com/embed/BV0fFtOsWuk?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
               figcaption Video. 
 
 
